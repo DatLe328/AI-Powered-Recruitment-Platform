06f7a9809296a4a3a05af4821a7bbd0c5aafc75b
DatLe328 <lvdat1705@gmail.com>
2025-08-19 12:18:51 +0700

    feat: restructure the project

--- README.md
+++
@@ -1,2 +0,0 @@
-# AI-Powered-Recruitment-Platform
-This project develops an AI-powered platform designed to efficiently connect employers with job seekers. It leverages advanced AI algorithms to streamline the recruitment process and ensure optimal matches.
--- description-parser/test.py
+++
@@ -1,5 +0,0 @@
-with open('data/cleaned_skills_3.txt', 'r', encoding='utf-8') as infile, \
-     open('cleaned_skills_3_out.txt', 'w', encoding='utf-8') as outfile:
-    for line in infile:
-        cleaned_line = line.strip().strip('"')
-        outfile.write(cleaned_line + '\n')
